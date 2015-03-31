0b2e70db4e8315670bee2674e24b05f205065bd5
Silvia T <tirca.silvia@gmail.com>
2015-03-31 17:25:47 +0300

    c

--- src/test/MainClass.java
+++ src/test/MainClass.java
@@ -3,6 +3,6 @@ package test;
 public class MainClass {
 	
 	public static void main(String[] args) {
-		System.out.println("Versionare Testing ..yfghftjh. ");
+		System.out.println("Versionare Testing ..yfgfhghftjh. ");
 	}
 }
